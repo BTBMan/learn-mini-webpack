50488f11024c44f01e550f3fb7242c4b01c5b66a
BTBMan <1498118157@qq.com>
2022-02-24 23:07:08 +0800

    babel的ast相关工具

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 import fs from 'fs';
 import parser from '@babel/parser';
+import traverse from '@babel/traverse';
 
 function createAsset() {
-  // 1. 读取入口文件 获取依赖的关系
+  // 1. 读取入口文件
   const source = fs.readFileSync('./example/main.js', {
     encoding: 'utf-8',
   });
 
-  console.log(source);
+  // 2.获取依赖的关系
+  // 首先通过文件内容解析为 ast
+  const ast = parser.parse(source, {
+    sourceType: 'module',
+  });
+
+  traverse.default(ast, {
+    enter(path) {
+      console.log(path);
+
+      if (path.isIdentifier({ name: 'n' })) {
+        path.node.name = 'x';
+      }
+    },
+  });
 }
 
 createAsset();
